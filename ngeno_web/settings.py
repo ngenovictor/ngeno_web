a08ae6d7d47dd73373f9a9a8fd6b9885c695feda
nongeveekay <ngenovictor321@gmail.com>
2015-03-31 10:22:01 +0300

    Finally my blog is up and running in my local server

--- ngeno_web/settings.py
+++ ngeno_web/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -58,8 +59,11 @@ WSGI_APPLICATION = 'ngeno_web.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'djangodb',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': ''
     }
 }
 
